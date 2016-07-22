731f8f97eb908d224c473df59df5e6a037f1cdb2
josh125 <jpdelossantos25@gmail.com>
2016-07-22 19:23:51 +0800

    change prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Required for Devise. Remember to change localhost:3000 to actual application host
-  config.action_mailer.default_url_options = { host: 'joshdemo-2016.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 end
